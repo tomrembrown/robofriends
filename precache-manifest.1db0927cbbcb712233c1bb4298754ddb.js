33b52e39fa50d1d5c7734f995f125fc9648a8bc4
Tom Brown <tombrown654321@protonmail.com>
2021-11-08 18:31:04 -0500

    Updates

---
+++ precache-manifest.1db0927cbbcb712233c1bb4298754ddb.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "515ecb59534233a85073547782ba8071",
+    "url": "/tomrembrown/robofriends/index.html"
+  },
+  {
+    "revision": "6a92b596df1021bd364e",
+    "url": "/tomrembrown/robofriends/static/css/2.038644eb.chunk.css"
+  },
+  {
+    "revision": "529c73381fa6f13db95d",
+    "url": "/tomrembrown/robofriends/static/css/main.cb59f067.chunk.css"
+  },
+  {
+    "revision": "6a92b596df1021bd364e",
+    "url": "/tomrembrown/robofriends/static/js/2.ad8d1974.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/tomrembrown/robofriends/static/js/2.ad8d1974.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "627485cada7abdc42d0d",
+    "url": "/tomrembrown/robofriends/static/js/3.c6b94e83.chunk.js"
+  },
+  {
+    "revision": "529c73381fa6f13db95d",
+    "url": "/tomrembrown/robofriends/static/js/main.3623eb9c.chunk.js"
+  },
+  {
+    "revision": "7ff6ec3de919fc63a0ec",
+    "url": "/tomrembrown/robofriends/static/js/runtime-main.e1a86273.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/tomrembrown/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
