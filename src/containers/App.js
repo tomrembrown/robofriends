2eaf4ab765ba114c54d90822a32e4022d44e7cf4
Tom Brown <tombrown654321@protonmail.com>
2021-11-28 13:54:23 -0500

    Added service workers

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,29 +1,55 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { requestRobots, setSearchField } from '../actions'
 import CardList from '../components/CardList'
 import ErrorBoundary from '../components/ErrorBoundary'
 import Scroll from '../components/Scroll'
 import SearchBox from '../components/SearchBox'
 import './App.css'
 
+// const mapStateToProps = (state) => {
+//   return {
+//     searchField: state.searchRobots.searchField,
+//     robots: state.requestRobots.robots,
+//     isPending: state.requestRobots.isPending,
+//     error: state.requestRobots.error,
+//   }
+// }
+
+// const mapDispatchToProps = (dispatch) => {
+//   return {
+//     onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+//     onRequestRobots: () => requestRobots(dispatch),
+//   }
+// }
+
 function App() {
-  const [searchField, setSearchField] = useState('')
+  const { searchField, isPending, robots, error } = useSelector((state) => ({
+    searchField: state.searchRobots.searchField,
+    isPending: state.requestRobots.isPending,
+    robots: state.requestRobots.robots,
+    error: state.requestRobots.error,
+  }))
 
-  const [robots, setRobots] = useState([])
+  const dispatch = useDispatch()
+
+  const onSearchChange = (event) => dispatch(setSearchField(event.target.value))
+
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) => setRobots(users))
+    dispatch(requestRobots())
+    // eslint-disable-next-line
   }, [])
-
-  const onSearchChange = (event) => {
-    setSearchField(event.target.value)
-  }
 
   const filteredRobots = robots.filter((robot) => {
     return robot.name.toLowerCase().includes(searchField.toLowerCase())
   })
-  return !robots.length ? (
+  return isPending ? (
     <h1>Loading</h1>
+  ) : error.length !== 0 ? (
+    <div className="tc">
+      <h1>Error</h1>
+      <h2> {error} </h2>
+    </div>
   ) : (
     <div className="tc">
       <h1 className="f1">RoboFriends</h1>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { applyMiddleware, combineReducers, createStore } from 'redux'
+import thunkMiddleware from 'redux-thunk'
 import 'tachyons'
 import App from './containers/App'
 import './index.css'
+import { requestRobots, searchRobots } from './reducers'
 import reportWebVitals from './reportWebVitals'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
+
+const rootReducer = combineReducers({ searchRobots, requestRobots })
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.unregister()
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
